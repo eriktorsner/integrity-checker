292589addc5696c9ec770892a123552432520cbf
Erik Torsner <erik@torgesta.com>
2017-01-09 17:57:16 +0100

    wip

--- tests/baseTest.php
+++ tests/baseTest.php
@@ -4,6 +4,6 @@ class baseTest extends PHPUnit_Framework_TestCase
 {
     public function testFoo()
     {
-        $this->assertEquals('hello', 'hello2');
+        $this->assertEquals('hello', 'hello');
     }
 }
