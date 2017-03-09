0b0a44f12711e17d1b8cbfa18d235731c62c9e93
Erik Torsner <erik@torgesta.com>
2017-03-09 19:09:23 +0100

    Adds test for adminUsername

--- tests/unit/Tests/SettingsTest.php
+++ tests/unit/Tests/SettingsTest.php
@@ -52,4 +52,37 @@ class SettingsTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($t['checkTablePrefix']['acceptable']);
     }
 
+    public function testAdminUsername()
+    {
+        \WP_Mock::userFunction('username_exists', array(
+            'args' => 'admin',
+            'return' => false,
+            'times' => 1,
+        ));
+        $dummy = new \stdClass();
+        $s = new Tests\Settings($dummy, new \MockState(), $dummy, $dummy);
+        $s->setBackgroundProcess(new \MockBackgroundProcess());
+        $s->adminUsername($dummy);
+        $t = $s->transientState;
+        print_r($t);
+        $this->assertTrue($t['adminUsername']['acceptable']);
+        $this->assertTrue(stripos($t['adminUsername']['result'], "does not exist") !==false);
+
+
+        \WP_Mock::userFunction('username_exists', array(
+            'args' => 'admin',
+            'return' => true,
+            'times' => 1,
+        ));
+        $dummy = new \stdClass();
+        $s = new Tests\Settings($dummy, new \MockState(), $dummy, $dummy);
+        $s->setBackgroundProcess(new \MockBackgroundProcess());
+        $s->adminUsername($dummy);
+        $t = $s->transientState;
+        print_r($t);
+        $this->assertFalse($t['adminUsername']['acceptable']);
+        $this->assertTrue(stripos($t['adminUsername']['result'], "Exists") !==false);
+
+    }
+
 }
