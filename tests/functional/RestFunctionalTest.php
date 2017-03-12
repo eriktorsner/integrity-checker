9d4a572676736c74aa284bb600aa0b5d856f554a
Erik Torsner <erik@torgesta.com>
2017-03-12 19:47:50 +0100

    Test

--- tests/functional/RestFunctionalTest.php
+++ tests/functional/RestFunctionalTest.php
@@ -11,7 +11,7 @@ class RestFunctionalTest extends \PHPUnit_Framework_TestCase
     {
         global $testUrl;
 
-        $ret = file_get_contents($testUrl);
+        $ret = file_get_contents($testUrl . '/index.php');
         $this->assertTrue(strpos($ret, 'Just another WordPress site') !== false);
     }
 }
