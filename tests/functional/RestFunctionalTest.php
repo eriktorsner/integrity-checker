2279edf0dae777945dec7f7bc841953925ece542
Erik Torsner <erik@torgesta.com>
2017-03-12 18:22:55 +0100

    Adds simple test to vefiry WP installation

--- tests/functional/RestFunctionalTest.php
+++ tests/functional/RestFunctionalTest.php
@@ -9,7 +9,9 @@ class RestFunctionalTest extends \PHPUnit_Framework_TestCase
 
     public function testRest1()
     {
+        global $testUrl;
+
+        $ret = file_get_contents($testUrl);
+        $this->assertTrue(strpos($ret, 'Just another WordPress site') !== false);
     }
-
-
 }
