82045d3f4820592fe8af0042337cdd07fa9160ea
Erik Torsner <erik@torgesta.com>
2017-03-13 11:58:20 +0100

    First proper live rest test

--- tests/functional/RestFunctionalTest.php
+++ tests/functional/RestFunctionalTest.php
@@ -7,19 +7,59 @@ class RestFunctionalTest extends \PHPUnit_Framework_TestCase
         setUpWp();
     }
 
-    public function testTemp()
-    {
-        //require_once ABSPATH . 'index.php';
-
-    }
-
-    public function testRest1()
+    public function testQuota()
     {
         global $testUrl;
 
-        $this->assertTrue(true);
-        //$ret = file_get_contents($testUrl);
-        //$this->assertTrue(strpos($ret, 'Just another WordPress site') !== false);
+        $ret = $this->restGet(
+            $testUrl . '/wp-json/integrity-checker/v1/quota'
+        );
 
+        $this->assertTrue(isset($ret['body']));
+        $body = $ret['body'];
+        $this->assertTrue(isset($body->code));
+        $this->assertEquals('success', $body->code);
+        $this->assertTrue(isset($body->data));
+        $this->assertEquals('ANONYMOUS', $body->data->validationStatus);
+
+    }
+
+    private function remotePost($url, $args)
+    {
+        $opts = array('http' =>
+                          array(
+                              'method'  => 'POST',
+                              'header'  => 'Content-type: application/x-www-form-urlencoded',
+                              'content' => isset($args['body'])? $args['body'] : null,
+                          )
+        );
+
+        $context  = stream_context_create($opts);
+        $content = file_get_contents($url, false, $context);
+        $codeParts = explode(' ', $http_response_header[0]);
+
+        return array(
+            'response' => array(
+                'code' => $codeParts[1],
+            ),
+            'body' => $content,
+        );
+    }
+
+    private function restGet($url, $args = null)
+    {
+        $content = @file_get_contents($url);
+        $codeParts = explode(' ', $http_response_header[0]);
+
+        if (json_decode($content)) {
+            $content = json_decode($content);
+        }
+
+        return array(
+            'response' => array(
+                'code' => $codeParts[1],
+            ),
+            'body' => $content,
+        );
     }
 }
