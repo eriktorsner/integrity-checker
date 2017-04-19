edb1085cd08cd3f2ea2dea1b494bf0931cd46bcd
Erik Torsner <erik@torgesta.com>
2017-04-19 17:40:40 +0200

    Moved mock version of WP_Rest_Response into it separate file

--- tests/MockObjects.php
+++ tests/MockObjects.php
@@ -321,16 +321,3 @@ class MockUpdatePlugins
         return $this->arr[$name];
     }
 }
-
-class WP_REST_Response
-{
-    public function __construct($data)
-    {
-        $this->data = $data;
-    }
-
-    public function header($name, $value)
-    {
-
-    }
-}
